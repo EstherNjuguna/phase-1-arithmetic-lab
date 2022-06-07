e3ce22915da7e16081917be6e8ef9200695f8879
+ <+>
2022-06-08 01:20:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1=31;
+const num2=2;
+const multiply= num1*num2;
+console.log (multiply);
+const num3= 25;
+const num4=7;
+const mod=num3%num4;
+console.log(mod);
+const max= Math.max(1, 2, 3, 4, 5,20);
+const random =Math.floor(Math.random() * 10) + 1;
